07b973775557699998dd53902cfd279909e77aa0
Beiby <sofia@gmail.com>
2024-11-27 15:34:49 -0500

    Se actualiza el enum StatusEnum y UbicacionEnum.

--- src/main/java/restaurante/example/demo/persistence/enums/StatusEnum.java
+++ src/main/java/restaurante/example/demo/persistence/enums/StatusEnum.java
@@ -2,7 +2,7 @@ package restaurante.example.demo.persistence.enums;
 
 // Enum que define los posibles estados de reserva de una mesa.
 public enum StatusEnum {
-    Disponible,  // Mesa disponible para ser ocupada
-    Ocupada,    // Mesa ocupada
-    Reservada   // Mesa reservada para un futuro cliente
+      DISPONIBLE,  // Mesa disponible para ser ocupada
+      OCUPADA,    // Mesa ocupada
+      RESERVADA   // Mesa reservada para un futuro cliente
 }
--- src/main/java/restaurante/example/demo/persistence/enums/UbicacionEnum.java
+++ src/main/java/restaurante/example/demo/persistence/enums/UbicacionEnum.java
@@ -2,7 +2,7 @@ package restaurante.example.demo.persistence.enums;
 
 // Enum que define los posibles lugares de ubicación de una mesa dentro del establecimiento.
 public enum UbicacionEnum {
-    Interior,   // Mesa ubicada en el interior del establecimiento
-    Exterior,   // Mesa ubicada en el exterior del establecimiento
-    Terraza     // Mesa ubicada en la terraza
+   INTERIOR ,   // Mesa ubicada en el interior del establecimiento
+   EXTERIOR ,   // Mesa ubicada en el exterior del establecimiento
+   TERRAZA     // Mesa ubicada en la terraza
 }
